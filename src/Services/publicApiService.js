66d08617f7300e7dfdf10116ceff657b8924d285
lautaropastor <lautaro.pastor@bbva.com>
2022-05-09 18:34:59 -0300

    Creación método Get

--- src/Services/publicApiService.js
+++ src/Services/publicApiService.js
@@ -2,12 +2,19 @@ import axios from 'axios';
 
 const config = {
     headers: {
-        Group: 01                //Aqui va el ID del equipo!!
+        Group: 2
     }
 }
 
-const Get = () => {
-    axios.get('https://jsonplaceholder.typicode.com/users', config)
+const Get = (endpoint, id = '') => {
+
+    const isUndefinedEndpoint = endpoint == null;
+
+    if(isUndefinedEndpoint) {
+        throw new Error("parameter 'endpoint' is not defined.");
+    }
+
+    axios.get(`https://ongapi.alkemy.org/api/${endpoint}/${id}`)
     .then(res => console.log(res))
     .catch(err => console.log(err))
 }
