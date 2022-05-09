7f012fe34bca9f9082ce6e3557bb54e0e6dcac7f
BrianMCiszewskiA <brianmatias.ciszewskialonso@bbva.com>
2022-05-09 17:15:57 -0300

    Agregado manejo de eeror de imagen. Nota: en el npm start no me cargaban las iamgenes locales usando relative path, usando src online si cargaban, quizas sea error del localhost

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,7 +1,22 @@
 function Card(props){
+    /* props a recibir:
+        imageSrc = src de la imagen.
+        name = Nombre de la card.
+        desc = Texto descriptivo de la card.
+        imgDimensions = dimension aprox de la img de la card, para usar los 
+            placeholders que tenemos en caso de que no cargue la img.
+    */
     return (
-        <div>
-            <img src={props.imageSrc} alt={props.name} className="card-img"></img>
+        <div className="card">
+            <img 
+                src={props.imageSrc} 
+                alt={props.name} 
+                className="card-img"
+                onError={({currentTarget}) => {
+                    currentTarget.onerror = false;
+                    currentTarget.src= "./public/images/placeholder/" + props.imgDimensions + ".png";
+                }}
+            />
             <h3 className="card-name">{props.name}</h3>
             <p className="card-desc">{props.desc}</p>
         </div>
