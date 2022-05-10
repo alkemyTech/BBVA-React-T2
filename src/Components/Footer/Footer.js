037fd7a873b11a79bd2a1fcf7f8f2587e4e2c2d5
Federico Amico <fede.amico98@gmail.com>
2022-05-10 15:19:27 -0300

    carpeta con estilo y jsx de footer creados

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <footer className="footer-container">
+            <p>hola</p>
+        </footer>
+    )
+}
+
+export default Footer;
