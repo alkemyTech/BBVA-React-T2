22579e263cca14210159a552a8f66f7a74cc7799
iara baya <iarabayav@gmail.com>
2022-05-10 11:17:03 -0300

    get function without authorization

--- src/Services/privateApiService.js
+++ src/Services/privateApiService.js
@@ -1,15 +1,25 @@
-import axios from 'axios';
+import axios from "axios";
+
+// const BASE_URL = process.env.BASE_URL;
+const BASE_URL = 'https://ongapi.alkemy.org/api';
 
 const config = {
-    headers: {
-        Group: 01                //Aqui va el ID del equipo!!
-    }
-}
+  headers: {
+    Group: 2 
+  },
+};
 
-const Get = () => {
-    axios.get('https://jsonplaceholder.typicode.com/users', config)
-    .then(res => console.log(res))
-    .catch(err => console.log(err))
-}
 
-export default Get
+const Get = async (endpoint, id = "") => {
+  
+  if(!endpoint) throw new Error("parameter 'endpoint' is not defined.");
+
+  // axios.defaults.headers.get['Authorization'] = getAuthorization();
+
+  const url = `${BASE_URL}/${endpoint}${ id? `/${id}`:''}` 
+  return axios.get(url, config)
+        .then((res) => res)
+        .catch((err) => err);
+};
+
+export default Get;
