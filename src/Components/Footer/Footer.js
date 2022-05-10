a63abaf5b33572d79b7be31876971dfdd598c814
Federico Amico <fede.amico98@gmail.com>
2022-05-10 15:33:31 -0300

    estructura de los estilos agregada

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -3,7 +3,19 @@ import './Footer.css';
 const Footer = () => {
     return (
         <footer className="footer-container">
-            <p>hola</p>
+            <div className='logo-container'>LOGO</div>
+            <ul className='list-container'>
+                <li className='list-container__li'>Inicio</li>
+                <li className='list-container__li'>Nosotros</li>
+                <li className='list-container__li'>Novedades</li>
+                <li className='list-container__li'>Testimonios</li>
+                <li className='list-container__li'>Contacto</li>
+                <li className='list-container__li'>Contribuye</li>
+            </ul>
+            <div className='circles-container'>CIRULOS</div>
+            <div className='copyright-container'>
+                <p>2022 by Alkemy. All rights reserved.</p>
+            </div>
         </footer>
     )
 }
