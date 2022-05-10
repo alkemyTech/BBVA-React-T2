de164666e011a524f8b77905ce08343de51d6056
Federico Amico <fede.amico98@gmail.com>
2022-05-10 14:19:25 -0300

    refactor funcion delete y llamado a la variable de entorno para obtener la url principal

--- src/Services/privateApiService.js
+++ src/Services/privateApiService.js
@@ -1,10 +1,12 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://jsonplaceholder.typicode.com';
+//const BASE_URL = 'https://jsonplaceholder.typicode.com';
+const BASE_URL = process.env.REACT_APP_BASE_URL;
 
 const config = {
     headers: {
         Group: 2                //Aqui va el ID del equipo!!
+        
     }
 }
 
@@ -16,15 +18,17 @@ const Get = () => {
 
 const Delete = (endpoint, id) => {
     const headers = getAuthorization();
+
     if(!endpoint) {
         throw new Error("parameter 'endpoint' is not definded");
     }
     if (id < 1) {
         throw new Error("parameter 'id' is invalid");
     }
-    axios.delete(`${BASE_URL}/${endpoint}/${id}`, { headers })
-    .then(res => { return res })
-    .catch(err => {return err })
+
+    axios.delete(`${BASE_URL}/${endpoint}/${id}`, headers )
+    .then(res => res )
+    .catch(err => err );
 }
 
 export { Delete }
