345bb152d113687840816dd313ff00bb7aba0bcc
lautaropastor <lautaro.pastor@bbva.com>
2022-05-10 17:15:47 -0300

    Creación método getMembers para obtener miembros del endpoint members

--- src/Components/Members/MembersList.js
+++ src/Components/Members/MembersList.js
@@ -1,11 +1,27 @@
 import { useState, useEffect } from 'react';
+import Get from '../../Services/publicApiService';
+import { Link } from 'react-router-dom';
 
 function MembersList() {
 
     const [members, setMembers] = useState([]);
 
+    useEffect(() => {
+        const getMembers = async () => {
+            const response = await Get('members');
+            const membersList = await response.data.data;
+            console.log(membersList);
+            setMembers([...membersList]);
+        }
+
+        getMembers();
+
+    }, [])
+
     return ( 
-        <>asd</>
+        <>
+            
+        </>
      );
 }
 
