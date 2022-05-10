953054bf45a57fdaa8a5509ae65210742f08be49
BrianCiszewski-bbva <brianmatias.ciszewskialonso@bbva.com>
2022-05-10 12:37:48 -0300

    Correccion del comentario

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -3,8 +3,6 @@ function Card(props){
         imageSrc = src de la imagen.
         name = Nombre de la card.
         desc = Texto descriptivo de la card.
-        imgDimensions = dimension aprox de la img de la card, para usar los 
-            placeholders que tenemos en caso de que no cargue la img.
     */
     return (
         <div className="card">
@@ -14,7 +12,7 @@ function Card(props){
                 className="card-img"
                 onError={({currentTarget}) => {
                     currentTarget.onerror = false;
-                    currentTarget.src= "https://uxwing.com/wp-content/themes/uxwing/download/07-web-app-development/image-not-found.svg"; //imagen online porque local me daba error
+                    currentTarget.src= "https://uxwing.com/wp-content/themes/uxwing/download/07-web-app-development/image-not-found.svg"; //imagen online porque local me daba error;
                 }}
             />
             <h3 className="card-name">{props.name}</h3>
