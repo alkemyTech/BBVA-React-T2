e2c7b30e07457194688e8534f54aefc361c3352b
lautaropastor <lautaro.pastor@bbva.com>
2022-05-10 16:14:44 -0300

    Creación estado members

--- src/Components/Members/MembersList.js
+++ src/Components/Members/MembersList.js
@@ -1,6 +1,11 @@
+import { useState, useEffect } from 'react';
+
 function MembersList() {
+
+    const [members, setMembers] = useState([]);
+
     return ( 
-        <></>
+        <>asd</>
      );
 }
 
