4ff7ac08e06486e382c3f9a7f9ebc4004859f4ba
lautaropastor <lautaro.pastor@bbva.com>
2022-05-10 19:56:27 -0300

    Creación componente MembersAbout

---
+++ src/Components/Members/MembersAbout.js
@@ -0,0 +1,11 @@
+import MembersList from "./MembersList";
+
+function MembersAbout() {
+    return ( 
+        <div>
+            <MembersList numberOfMembers={6}/>
+        </div>
+     );
+}
+
+export default MembersAbout;
