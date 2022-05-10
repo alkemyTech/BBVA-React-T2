6e973f97a5f0ace2b17bf42e02ccc8fddfb47c1a
Julia Scodelari <j.scodelari96@gmail.com>
2022-05-10 14:23:10 -0300

    solucion conflictos

--- src/Services/publicApiService.js
+++ src/Services/publicApiService.js
@@ -1,21 +1,31 @@
 import axios from 'axios';
 
-const url = 'https://ongapi.alkemy.org/api/'
-
 const config = {
     headers: {
-        Group: 02               //Aqui va el ID del equipo!!
+        Group: 2
     }
 }
+const BASE_URL = "https://ongapi.alkemy.org/api"; // Provisorio hasta tener el archivo .env
+
+const Get = async (endpoint, id = '') => {
+
+    if(!endpoint) {
+        throw new Error("parameter 'endpoint' is not defined.");
+    }
+
+    try {
+        const url = `${BASE_URL}/${endpoint}/${id}`;
+        const response = await axios.get(url);
+        return response; 
+    } catch (error) {
+        return error;
+    }
+}  
 
 const Post = (endpoint, body) => {
-    axios.post(url + endpoint, body)
-    .then((res) => {
-        return(res)
-    })
-    .catch((err) => {
-        return(err)
-    })
+    axios.post(BASE_URL + endpoint, body)
+    .then(res => res)
+    .catch(err => err)
 }
 
-export { Post }
+export { Post, Get }
