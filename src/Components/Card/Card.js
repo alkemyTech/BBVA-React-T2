6a4c631b416f5cedd4bf1be7470439026ba02f7c
BrianCiszewski-bbva <brianmatias.ciszewskialonso@bbva.com>
2022-05-10 13:28:30 -0300

    Correcciones del pr hechas: pase a arrow function y desestructuracion de props

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,5 +1,5 @@
-function Card(props){
-    /* props a recibir:
+const Card({imageSrc, name, desc}) => {
+    /*
         imageSrc = src de la imagen.
         name = Nombre de la card.
         desc = Texto descriptivo de la card.
@@ -7,16 +7,16 @@ function Card(props){
     return (
         <div className="card">
             <img 
-                src={props.imageSrc} 
-                alt={props.name} 
+                src={imageSrc} 
+                alt={name} 
                 className="card-img"
                 onError={({currentTarget}) => {
                     currentTarget.onerror = false;
                     currentTarget.src= "https://uxwing.com/wp-content/themes/uxwing/download/07-web-app-development/image-not-found.svg"; //imagen online porque local me daba error;
                 }}
             />
-            <h3 className="card-name">{props.name}</h3>
-            <p className="card-desc">{props.desc}</p>
+            <h3 className="card-name">{name}</h3>
+            <p className="card-desc">{desc}</p>
         </div>
     )
 }
