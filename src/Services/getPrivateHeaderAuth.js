7f8e64dfad5c03e0131fcc73b86111473f423c83
BrianCiszewski-bbva <brianmatias.ciszewskialonso@bbva.com>
2022-05-11 11:33:23 -0300

    Cambio de Header() a {}

--- src/Services/getPrivateHeaderAuth.js
+++ src/Services/getPrivateHeaderAuth.js
@@ -1,11 +1,10 @@
 const getPrivateHeaderAuth = () => {
     //Devuelve un objeto header con la autorizacion si es que hay un token valido
     token = getToken();
-    header = new Headers();
-    if(token){ //deberia comprobar en el if si el token es valido para el modo privado?
-        header.append('Authorization', 'Bearer ' + token);
+    header = {};
+    if(token){
+        header = {'Authorization': 'Bearer ' + token};
     }
-    //deberia tirar error en caso de que no haya un token?
     return header;
 }
 
