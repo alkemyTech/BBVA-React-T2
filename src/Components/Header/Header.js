7785644269743e1ab7694d0f807155876831ad3c
variniaquisbert <variniaminerva.quisbertsoria@bbva.com>
2022-05-11 11:53:19 -0300

    Header component image format edited | png to svg

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,21 +2,20 @@ import { useState } from 'react';
 // Material UI components
 import MenuIcon from '@mui/icons-material/Menu';
 
-import './Header.css'
+import './Header.css';
 
 export default function Header() {
 
     const [isMenuIconClicked, setIsMenuIconClicked] = useState(false);
 
     const handleClick = () => {
-        // here we handle button click logic
         ((!isMenuIconClicked) ? setIsMenuIconClicked(true) : setIsMenuIconClicked(false))
     }
 
     return (
         <header className='header-container'>
             <MenuIcon fontSize="large" onClick={handleClick} />
-            <img src='/logo.png' alt='logo-ONG' />
+            <img src='/logo.svg' alt='logo-ONG' />
         </header>
     )
 }
