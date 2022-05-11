faa87ee711d5342f1b6115779d63a4c2c874293c
iara baya <iarabayav@gmail.com>
2022-05-11 10:49:32 -0300

    contact data added

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -13,6 +13,17 @@ const Contact = () => {
       </button>
 
       <h1>¡Contactate con nosotros!</h1>
+
+      <div>
+        <h4>Nuestros datos de contacto:</h4>
+        <ul>
+          <li>Email: somosfundacionmas@gmail.com</li>
+          <li>Instagram: SomosMás</li>
+          <li>Facebook: Somos_Más</li>
+          <li>Teléfono de contacto: 1160112988</li>
+        </ul>
+      </div>
+
       <ContactForm/>
 
       <button type='button'>
