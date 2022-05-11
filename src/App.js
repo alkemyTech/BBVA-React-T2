366aa1f1c6f6042b775d334b4e58cfb9d9d18809
Julia Scodelari <j.scodelari96@gmail.com>
2022-05-11 15:25:36 -0300

    renderizacion de datos de usuarios

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import ToysCampaign from './Campaigns/Toys/ToysCampaign';
 import MembersForm from './Components/Members/MembersForm';
 import ProjectsForm from './Components/Projects/ProjectsForm';
 import About from './about/About';
+import UserList from './Components/Users/UserList';
 
 function App() {
   return (
@@ -32,6 +33,7 @@ function App() {
           <Route path="/school-campaign" component={SchoolCampaign} />
           <Route path="/toys-campaign" component={ToysCampaign} />
           <Route path="/about" component={About} />
+          <Route path="/backoffice/users" component={UserList} />
         </Switch>
       </BrowserRouter>
     <div className="App">
--- src/Components/Users/UserList.js
+++ src/Components/Users/UserList.js
@@ -6,13 +6,25 @@ const UserList = () => {
     const [userList, setUserList] = useState([])
 
     const fetchData = async () => {
-        const res = await Get("users");
-        console.log(res)
+        const res = await Get("/users");
+        setUserList(res.data)
+        console.log(userList)
       };
+
+      useEffect(() => {
+        fetchData();
+      }, []);
     
     return(
         <>
             <h1>Holo</h1>
+            <ul>
+                {userList.map( (user) =>{
+                    return(
+                        <li key={user.id}>{user.name}</li>
+                    )
+                })}
+            </ul>
         </>
     )
 }
