e59c26f3b7d62b7edd500f5fe554e61e2b6a9043
BrianCiszewski-bbva <brianmatias.ciszewskialonso@bbva.com>
2022-05-12 11:58:14 -0300

    Estilizado el spinner

--- src/features/Spinner/Spinner.js
+++ src/features/Spinner/Spinner.js
@@ -1,8 +1,14 @@
-
+import * as React from 'react';
+import CircularProgress from '@mui/material/CircularProgress';
+import './Spinner.css'
 
 const Spinner = () => {
 
-    return
+    return (
+        <div className='SpinnerDiv'>
+            <CircularProgress/>
+        </div>
+    )
 }
 
 export default Spinner;
