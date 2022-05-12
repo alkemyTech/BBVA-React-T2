aaab3757181cbd4f8ba5dd33146d18cc2ebfd6ee
Federico Amico <fede.amico98@gmail.com>
2022-05-12 16:09:34 -0300

    llamado a privateService para conusmir el método get y obtener los datos ed la empresa. También plasmo estos datos dentro del DOM

--- src/Components/Organization/Organization.js
+++ src/Components/Organization/Organization.js
@@ -1,10 +1,34 @@
 /*
 Al ingresar a la ruta /backoffice/organization se mostrarán los datos actuales de la Organización 
 (name, image, shortDescription), y la acción para acceder al formulario de edición (/backoffice/organization/edit)*/
+import { useEffect, useState } from 'react';
+import { Get } from '../../Services/privateApiService';
+import './Organization.css';
 
-const Organization = () => {
+const Organization = () => { 
+    const [information, setInformation] = useState({});
+
+    const getData = async () => {
+        try {
+            let res = await Get('/organization');
+            setInformation(res.data.data)
+        } catch (err) {
+            alert('Hubo un error')
+            return err;
+        }
+    }
+    
+    useEffect(() => {
+        getData()
+    } , []);
+
+
     return (
-        <h1>Organizacion</h1>
+        <div className="organization-container">
+            <h1>{information.name}</h1>
+            <img src={information.logo} alt='imagen ong' />
+            <p>{information.short_description}</p>
+        </div>
     )
 }
 
