25b6889a0642dcfb5a3bb2ef920100e6299ae9cb
lautaropastor <lautaro.pastor@bbva.com>
2022-05-12 18:27:42 -0300

    Estructura HTML listado Sidebar

--- src/Components/Sidebar/Sibebar.js
+++ src/Components/Sidebar/Sibebar.js
@@ -1,11 +1,33 @@
+import './Sidebar.css';
+import { useState } from 'react';
 function Sidebar() {
+
+    const [bool, setBool] = useState(false);
+
     return ( 
         <>
-            <button className="sidebar-menu">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h7" />
+            <button className="sidebar-menu" onClick={() => setBool(!bool)}>
+                { bool || 
+                <svg className="sidebar-menu__icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h7" />
                 </svg>
+                }
+                { bool && 
+                <svg className="sidebar-menu__icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h8m-8 6h16" />
+                </svg>
+                }
             </button>
+            <ul>
+                <li>Usuarios</li>
+                <li>Organización</li>
+                <li>Miembros</li>
+                <li>Actividades</li>
+                <li>Testimonios</li>
+                <li>Novedades</li>
+                <li>Categorías</li>
+                <li>Slides</li>
+            </ul>
         </>
      );
 }
