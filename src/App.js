4f16d529e839090ea29e24da60f2e67da84c2f8f
BrianCiszewski-bbva <brianmatias.ciszewskialonso@bbva.com>
2022-05-12 15:45:15 -0300

    preparado path para edicion de actividades

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ function App() {
         <Switch>
           {/* <Route path="/" exact component={} />           Esta ruta debe ser para el Home */}
           <Route path="/create-activity" component={ActivitiesForm} />
+          <Route path="/backoffice/edit-activity/:id" component={ActivitiesForm}/>
           <Route path="/create-category" component={CategoriesForm} />
           <Route path="/create-news" component={NewsForm} />
           <Route path="/backoffice/create-slide" component={SlidesForm} />
