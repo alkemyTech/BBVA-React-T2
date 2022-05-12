2244ec28efe6f3d4abe652e38095b7809cf491de
BrianCiszewski-bbva <brianmatias.ciszewskialonso@bbva.com>
2022-05-12 15:03:55 -0300

    Agregado input basico para imagen

--- src/Components/Activities/ActivitiesForm.js
+++ src/Components/Activities/ActivitiesForm.js
@@ -4,12 +4,15 @@ import '../FormStyles.css';
 const ActivitiesForm = () => {
     const [initialValues, setInitialValues] = useState({
         name: '',
+        image: '',
         description: ''
     });
 
     const handleChange = (e) => {
         if(e.target.name === 'name'){
             setInitialValues({...initialValues, name: e.target.value})
+        }if(e.target.name === 'image'){
+            setInitialValues({...initialValues, image: e.target.value})
         } if(e.target.name === 'description'){
             setInitialValues({...initialValues, description: e.target.value})
         }
@@ -23,6 +26,7 @@ const ActivitiesForm = () => {
     return (
         <form className="form-container" onSubmit={handleSubmit}>
             <input className="input-field" type="text" name="name" value={initialValues.name} onChange={handleChange} placeholder="Activity Title"></input>
+            <input className="input-field" type="text" name="image" value={initialValues.image} onChange={handleChange} placeholder="Activity Image"></input>
             <input className="input-field" type="text" name="description" value={initialValues.description} onChange={handleChange} placeholder="Write some activity description"></input>
             <button className="submit-btn" type="submit">Send</button>
         </form>
