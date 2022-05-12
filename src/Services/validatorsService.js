111743d0a336a98366c263ecac80452d316f4697
lautaropastor <lautaro.pastor@bbva.com>
2022-05-12 14:20:40 -0300

    Creación validatorsService + estilos generales

---
+++ src/Services/validatorsService.js
@@ -0,0 +1,35 @@
+export function validateLength(field = '', minlength = 0) {
+    return field.length >= minlength ?  true : false;
+  }
+
+export function validateImageFormat(imageUrl = '') {
+
+    if (imageUrl.length < 5) {
+      return false;
+    }
+
+    // const lastFourChar = `${imageUrl[imageUrl.length - 4]}${imageUrl[imageUrl.length - 3]}${imageUrl[imageUrl.length - 2]}${imageUrl[imageUrl.length - 1]}`;
+    let lastFourChar = '';
+    for (let i = 4; i > 0; i--) {
+      lastFourChar += imageUrl[imageUrl.length - i];
+    }
+
+    const isPng = lastFourChar === '.png';
+    const isJpg = lastFourChar === '.jpg';
+
+    if (isPng || isJpg) {
+      return true;
+    } else {
+      return false;
+    }
+
+}
+
+export function validateHasContent(field = '') {
+    const fieldWithoutSpaces = field.trim();
+    return fieldWithoutSpaces.length > 0 ?  true : false;
+}
+
+export function validateSocialNetworkUrl(socialNetwork = '', url = '') {
+    
+}
