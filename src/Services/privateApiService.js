98872f8ac76b7872dcf13baee9e9543beac9f2dc
Gustavo27Gonzalez <gustavo27gonzalez27@gmail.com>
2022-05-12 15:04:43 -0300

    creando peticion put a los endpoints privados

--- src/Services/privateApiService.js
+++ src/Services/privateApiService.js
@@ -1,15 +1,27 @@
 import axios from 'axios';
+import { getPrivateHeaderAuth } from '../Services/'
+
+const BASE_URL = process.env.REACT_APP_BASE_URL;
 
 const config = {
     headers: {
-        Group: 01                //Aqui va el ID del equipo!!
+        Group: 2               //Aqui va el ID del equipo!!
     }
 }
 
-const Get = () => {
-    axios.get('https://jsonplaceholder.typicode.com/users', config)
-    .then(res => console.log(res))
-    .catch(err => console.log(err))
-}
-
-export default Get
+const Put = (endpoint, id, body) => {
+    const headers = getPrivateHeaderAuth();
+  
+    if(!endpoint) {
+        throw new Error("parameter 'endpoint' is not definded");
+    }
+    if (id < 1) {
+        throw new Error("parameter 'id' is invalid");
+    }
+  
+    axios.put(`${BASE_URL}/${endpoint}/${id}`, body, headers)
+    .then(res => res )
+    .catch(err => err );
+  }
+  
+  export {Delete, Get, Put}
