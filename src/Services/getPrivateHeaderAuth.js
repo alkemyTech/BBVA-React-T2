9eb0b034f4240d82f45fe1af9e5e566ca677dfa6
BrianCiszewski-bbva <brianmatias.ciszewskialonso@bbva.com>
2022-05-12 11:17:22 -0300

    agregado manejo de error

--- src/Services/getPrivateHeaderAuth.js
+++ src/Services/getPrivateHeaderAuth.js
@@ -2,12 +2,9 @@ import getToken from './getToken';
 
 const getPrivateHeaderAuth = () => {
     //Devuelve un objeto header con la autorizacion si es que hay un token valido
-    token = getToken();
-    header = {};
-    if(token){
-        header = {'Authorization': 'Bearer ' + token};
-    }
-    return header;
+    const token = getToken();
+
+    return token ? {'Authorization': 'Bearer ' + token} : {error: "No token found"};
 }
 
 export default getPrivateHeaderAuth
