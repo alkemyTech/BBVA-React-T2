f2cc65527fac5650609b9a7e2c8ebb34fc878177
iara baya <iarabayav@gmail.com>
2022-05-13 01:06:11 -0300

    agregado estilos a la pantalla y formulario en general

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -2,11 +2,27 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import './Contact.css'
 import ContactForm from './ContactForm'
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import { TextField } from '@mui/material';
 
 const Contact = (props) => {
+  const { 
+    address, 
+    instagram_url, 
+    facebook_url, 
+    linkedin_url, 
+    twitter_url, 
+    phone } = props
+
+    const iconProp = { fontSize: 40, margin:1 }
+
+
   return (
-    <>
-      <h2 className='title'>¿Queres contribuir con { props.name }?</h2>
+    <div className='container'>
+      <h2 className='title'>¿Queres contribuir?</h2>
 
       <button type='button' className='primary-button'>
         <Link to='/' className='contact__link'>
@@ -16,23 +32,38 @@ const Contact = (props) => {
 
       <h2 className='title'>¡Contactate con nosotros!</h2>
 
-      <div>
-        <h4>Nuestros datos de contacto son:</h4>
-        <ul>
-          <li>Direccion: { props.address } </li>
-          <li>Instagram: { props.instagram_url } </li>
-          <li>Facebook: { props.facebook_url } </li>
-          <li>Linkedin: { props.linkedin_url } </li>
-          <li>Twitter: { props.twitter_url } </li>
-          <li>Teléfono de contacto: { props.phone} </li>
+      <div className='contact__data-section'>
+          <p>Direccion: { address || 'direccion' } </p>
+          <p>Teléfono de contacto: { phone || 'telefono' } </p>
+
+        <ul className='contact__media-list'>
+          <li className='contact__media-list-item'>
+            <InstagramIcon sx={iconProp}/> 
+            { instagram_url || 'instagram_url'}
+          </li>
+
+          <li className='contact__media-list-item'>
+            <FacebookIcon sx={iconProp} color="primary"/> 
+            { facebook_url || 'facebook_url'}
+          </li>
+
+          <li className='contact__media-list-item'>
+            <LinkedInIcon sx={iconProp} color="primary"/> 
+            { linkedin_url || 'linkedin_url'}
+          </li>
+
+          <li className='contact__media-list-item'>
+            <TwitterIcon sx={iconProp} color="primary"/>
+            { twitter_url || 'twitter_url'} 
+          </li>
         </ul>
       </div>
 
       <h2 className='title' >Puedes dejarnos tu consulta:</h2>
-
+     
       <ContactForm/>
 
-    </>
+    </div>
   )
 }
 
--- src/Components/Contact/ContactForm.js
+++ src/Components/Contact/ContactForm.js
@@ -19,52 +19,59 @@ const ContactForm = () => {
     }) );
   };
 
+  const inputProps = { style: {fontSize: 20} }
+  const inputHelperProps = { style: {fontSize: 15} }
 
   return (
-    <form className='contact-form' onSubmit={handleSubmit}>
-     
+    <form className='contact__form' onSubmit={handleSubmit}>
+      
       <TextField
         helperText="Introduce tu nombre completo"
-        label='Nombre'
+        FormHelperTextProps={inputHelperProps}
+        InputLabelProps={inputProps}
+        InputProps={inputProps}
+        label='Nombre y Apellido'
         name='name'
         variant='outlined'
         value={formData.name}
         onChange={handleChange}
       />
-      <br/>
 
       <TextField
         helperText="Introduce tu correo electronico"
+        FormHelperTextProps={inputHelperProps}
+        InputLabelProps={inputProps}
+        InputProps={inputProps}
         label='Email'
         name='email'
         variant='outlined'
         value={formData.email}
         onChange={handleChange}
       />
-      <br/>
-      
+
       <TextField
+        InputLabelProps={inputProps}
+        InputProps={inputProps}
         label='Telefono'
         name='phone'
         variant='outlined'
         value={formData.phone}
         onChange={handleChange}
       />
-      <br/>
 
       <TextField
+        InputLabelProps={inputProps}
+        InputProps={inputProps}
         multiline
         label='Escribe tu consulta'
-        placeholder='Escribe tu consulta'
         name='message'
         variant='outlined'
         rows={4}
         value={formData.message}
         onChange={handleChange}
       />
-      <br/>
 
-      <button type='submit' className='secondary-button'>
+      <button type='submit' className='secondary-button contact__send-button'>
         Enviar consulta
       </button>
 
