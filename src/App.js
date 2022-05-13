61228ca3383da16d37ad1d61459516b53a180e9a
iara baya <iarabayav@gmail.com>
2022-05-13 12:32:41 -0300

    componente layout agregado anidando el componente Switch con rutas

--- src/App.js
+++ src/App.js
@@ -25,20 +25,22 @@ function App() {
   return (
     <>
       <BrowserRouter>
-        <Switch>
-          {/* <Route path="/" exact component={} />           Esta ruta debe ser para el Home */}
-          <Route path="/create-activity" component={ActivitiesForm} />
-          <Route path="/create-category" component={CategoriesForm} />
-          <Route path="/create-news" component={NewsForm} />
-          <Route path="/backoffice/create-slide" component={SlidesForm} />
-          <Route path="/create-testimonials" component={TestimonialForm} />
-          <Route path="/create-user" component={UserForm} />
-          <Route path="/create-member" component={MembersForm} />
-          <Route path="/create-project" component={ProjectsForm} />
-          <Route path="/school-campaign" component={SchoolCampaign} />
-          <Route path="/toys-campaign" component={ToysCampaign} />
-          <Route path="/about" component={About} />
-        </Switch>
+        <Layout>
+          <Switch>
+            {/* <Route path="/" exact component={} />           Esta ruta debe ser para el Home */}
+            <Route path="/create-activity" component={ActivitiesForm} />
+            <Route path="/create-category" component={CategoriesForm} />
+            <Route path="/create-news" component={NewsForm} />
+            <Route path="/backoffice/create-slide" component={SlidesForm} />
+            <Route path="/create-testimonials" component={TestimonialForm} />
+            <Route path="/create-user" component={UserForm} />
+            <Route path="/create-member" component={MembersForm} />
+            <Route path="/create-project" component={ProjectsForm} />
+            <Route path="/school-campaign" component={SchoolCampaign} />
+            <Route path="/toys-campaign" component={ToysCampaign} />
+            <Route path="/about" component={About} />
+          </Switch>
+        </Layout>
       </BrowserRouter>
     </>
   );
