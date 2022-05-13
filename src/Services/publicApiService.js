bbad16aedb886aaeef451b6cd8e7d9e896dcdcba
Julia Scodelari <j.scodelari96@gmail.com>
2022-05-13 10:16:31 -0300

    arreglo url de los metodos public

--- src/Services/publicApiService.js
+++ src/Services/publicApiService.js
@@ -8,14 +8,10 @@ const config = {
 
 const BASE_URL = process.env.REACT_APP_BASE_URL;
 
-const Get = async (endpoint, id = '') => {
-
-    if(!endpoint) {
-        throw new Error("parameter 'endpoint' is not defined.");
-    }
+const Get = async (endpoint) => {
 
     try {
-        const url = `${BASE_URL + endpoint + '/' + id}`;
+        const url = `${BASE_URL + endpoint}`;
         const response = await axios.get(url);
         return response; 
     } catch (error) {
