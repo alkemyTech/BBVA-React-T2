bbb0e97fb464fbe28b1368b022e70adcde5e5a8f
Julia Scodelari <j.scodelari96@gmail.com>
2022-05-13 16:32:27 -0300

    borrado de codigo comentado

--- src/App.js
+++ src/App.js
@@ -1,16 +1,6 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-// import ActivitiesForm from './Components/Activities/ActivitiesForm';
-// import CategoriesForm from './Components/Categories/CategoriesForm';
-// import NewsForm from './Components/News/NewsForm';
-// import SlidesForm from './Components/Slides/SlidesForm';
-// import TestimonialForm from './Components/Testimonials/TestimonialsForm';
-// import UserForm from './Components/Users/UsersForm';
-// import SchoolCampaign from './Campaigns/School/SchoolCampaign';
-// import ToysCampaign from './Campaigns/Toys/ToysCampaign';
-// import MembersForm from './Components/Members/MembersForm';
-// import ProjectsForm from './Components/Projects/ProjectsForm';
 import About from './Components/Frontoffice/About/About'
 import UserList from './Components/Users/UserList';
 
@@ -19,17 +9,6 @@ function App() {
     <>
       <BrowserRouter>
         <Switch>
-          {/* <Route path="/" exact component={} />           Esta ruta debe ser para el Home */}
-          {/* <Route path="/create-activity" component={ActivitiesForm} />
-          <Route path="/create-category" component={CategoriesForm} />
-          <Route path="/create-news" component={NewsForm} />
-          <Route path="/backoffice/create-slide" component={SlidesForm} />
-          <Route path="/create-testimonials" component={TestimonialForm} />
-          <Route path="/create-user" component={UserForm} />
-          <Route path="/create-member" component={MembersForm} />
-          <Route path="/create-project" component={ProjectsForm} />
-          <Route path="/school-campaign" component={SchoolCampaign} />
-          <Route path="/toys-campaign" component={ToysCampaign} /> */}
           <Route path="/about" component={About} />
           <Route exact path="/backoffice/users" component={UserList} />
         </Switch>
