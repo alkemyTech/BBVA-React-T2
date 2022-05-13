b0e1854ea26786226fc34b2457d0ee1fa38d0149
Federico Amico <fede.amico98@gmail.com>
2022-05-13 10:30:47 -0300

    delete de actividades funciona

--- src/Components/Activities/Acitivity.js
+++ src/Components/Activities/Acitivity.js
@@ -7,7 +7,7 @@ const Activity = ({ id, name, image, createdAt, handleDelete, handleEdit }) => {
             <img src={image} alt='imagen de la actividad' className='activity-container__img'/>
             <p className='activity-container-p__createAt'>{createdAt}</p>
             <div className='button-container'>
-                <button type='button' className='primary-backoffice-button button-container__edit' onClick={handleEdit}>Editar</button>
+                <button type='button' className='primary-backoffice-button button-container__edit' onClick={()=>handleEdit(id)}>Editar</button>
                 <button type='button' className='button-container__delete secondary-backoffice-button' onClick={()=>handleDelete(id)}>Eliminar</button>
             </div>
         </div>
--- src/Components/Activities/ActivitiesBackOffice.js
+++ src/Components/Activities/ActivitiesBackOffice.js
@@ -3,23 +3,37 @@ El mismo tendrá datos mockeados para representar los datos, que posteriormente 
 La tabla mostrará los campos name, image, y createdAt, y las acciones para eliminar y editar. 
 En la sección superior, mostrará un componente <Link> que redirigirá a la ruta /backoffice/activities/create.*/
 import './ActivitiesBackOffice.css';
+import { useState, useEffect } from 'react';
 import Activity from './Acitivity';
 import '../../general-styles.css';
-//import { Delete } from '../../Services/privateApiService';
+import { Delete } from '../../Services/privateApiService';
 
 const ActivitiesBackOffice = () => {
+    const [activitiesMock, setActivitiesMock] = useState([])
+    
+    //cada vez que se elimina una tarea se actualiza el listado de actividades. Aca dentro iría la llamada al endpoint getActivities.
+    useEffect(() => {
+        setActivitiesMock([
+            {id: 1, name: 'actividad 1', imagen:'imagen1', createdAt: '12/01/2022'},
+            {id: 2, name: 'actividad 2', imagen:'imagen2', createdAt: '12/01/2022'},
+            {id: 3, name: 'actividad 3', imagen:'imagen3', createdAt: '12/01/2022'},
+            {id: 4, name: 'actividad 4', imagen:'imagen4', createdAt: '12/01/2022'}
+        ]);
+    },[])
 
-    let activitiesMock = [
-        {id: 1, name: 'actividad 1', imagen:'imagen1', createdAt: '12/01/2022'},
-        {id: 2, name: 'actividad 2', imagen:'imagen2', createdAt: '12/01/2022'},
-        {id: 3, name: 'actividad 3', imagen:'imagen3', createdAt: '12/01/2022'},
-        {id: 4, name: 'actividad 4', imagen:'imagen4', createdAt: '12/01/2022'}
-    ];
-
-    const handleEdit = () => {}
-    const handleDelete = (id) => {
-        console.log(id);
-        //Delete(process.env.REACT_APP_ACTIVITIES, id)
+    const handleEdit = (id) => {
+        console.log(id)
+    }
+    const handleDelete = async (id) => {
+        try {
+            await Delete(process.env.REACT_APP_ACTIVITIES, id);
+            //aca se debe consultar el endpoint de actividades de nuevo para actualizarlo.
+            //se reemplazaría el activitiesMock.filter por GET(Activities)
+            let activitiesUpdated = await activitiesMock.filter(activity => activity.id !== id);
+            setActivitiesMock(activitiesUpdated);
+        } catch (err) {
+            alert('error')
+        }
     }
 
     return (
