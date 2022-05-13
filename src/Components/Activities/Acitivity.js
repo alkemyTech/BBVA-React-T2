60a74476e53616a9d06d7c0a4935a1e38bfc6c46
Federico Amico <fede.amico98@gmail.com>
2022-05-13 13:21:03 -0300

    estilos responsive arreglado

--- src/Components/Activities/Acitivity.js
+++ src/Components/Activities/Acitivity.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const Activity = ({ id, name, image, createdAt, handleDelete }) => {
     return (
-        <div key = {id} className='activity-container rows-container'>
+        <div className='activity-container rows-container'>
             <p className='activity-container-p__name'>{name}</p>
             <img src={image} alt='imagen de la actividad' className='activity-container__img'/>
             <p className='activity-container-p__createAt'>{createdAt}</p>
