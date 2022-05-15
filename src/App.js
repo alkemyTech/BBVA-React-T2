2058ff3e809bf75c0df8817647d7bd5991ae555f
Federico Amico <fede.amico98@gmail.com>
2022-05-15 15:53:46 -0300

    arreglo de errores de la branch anterior. Componente funcionando correctamente y actualizado con lo último

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import About from './Components/Frontoffice/About/About'
 import UserList from './Components/Users/UserList';
+import Organization from './Components/Backoffice/Organization/Organization';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Switch>
           <Route path="/about" component={About} />
           <Route exact path="/backoffice/users" component={UserList} />
+          <Route exact path="/backoffice/organization" component={Organization} />
         </Switch>
       </BrowserRouter>
     </>
