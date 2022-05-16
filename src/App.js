841012e8132b4de5c73d878625932117da9e75a6
Julia Scodelari <j.scodelari96@gmail.com>
2022-05-16 10:38:50 -0300

    integracion con componente UsersList

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import About from './Components/Frontoffice/About/About'
 import UserForm from './Components/Backoffice/Users/UsersForm';
+import UserList from './Components/Backoffice/Users/UserList';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path="/about" component={About} />
           <Route path="/backoffice/users/create" component={UserForm} />
           <Route path="/backoffice/users/edit/:id" component={UserForm} />
+          <Route exact path="/backoffice/users" component={UserList} />
         </Switch>
       </BrowserRouter>
     </>
