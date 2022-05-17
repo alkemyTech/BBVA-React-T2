cb0907b8c2f51322da1d778c1518b51ccf6a645a
Federico Amico <fede.amico98@gmail.com>
2022-05-17 11:48:45 -0300

    proceso de obtencion de token y validacion del rol del usuario

--- src/Components/Backoffice/RoutesSecurity/RoutesSecurity.js
+++ src/Components/Backoffice/RoutesSecurity/RoutesSecurity.js
@@ -1,15 +1,26 @@
 import getPrivateHeaderAuth from '../../../Services/getPrivateHeaderAuth.js';
 import { Get } from '../../../Services/privateApiService';
 
-//const endpoint = process.env.
+const endpoint = process.env.REACT_APP_BASE_URL + process.env.REACT_APP_AUTH
+
+const isAdmin = (role_id) => {
+    if(role_id === 1) {return true;} else {return false;}
+}
 
 const checkRole = async () => {
     try {
         const token = await getPrivateHeaderAuth();
-        
+        const res = await Get(endpoint);
+        if (isAdmin(res.data.data.user.role_id)){
+            return true 
+        } else {
+            return false
+        };
     } catch (err) {
-        console.log(err)
+        return err;
     }
 }
 
-export default checkRole;
+checkRole()
+
+export { checkRole };
