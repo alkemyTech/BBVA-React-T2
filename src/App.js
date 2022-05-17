474eec9e0e24da0001d7905ec1fefb10af56a9d2
iara baya <iarabayav@gmail.com>
2022-05-17 10:15:57 -0300

    cambios en el switch de app.js

--- src/App.js
+++ src/App.js
@@ -1,35 +1,37 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 //Public
-import Layout from './Components/Layout/Layout';
-import About from './Components/Frontoffice/About/About'
-import Contact from './Components/Frontoffice/Contact/Contact';
+import Layout from "./Components/Layout/Layout";
+import About from "./Components/Frontoffice/About/About";
+import Contact from "./Components/Frontoffice/Contact/Contact";
 
 //Backoffice
-import BackofficeLayout from './Components/Layout/BackofficeLayout';
-import UserList from './Components/Backoffice/Users/UserList';
-import OrganizationForm from './Components/Backoffice/Organization/OrganizationForm';
+import BackofficeLayout from "./Components/Layout/BackofficeLayout";
+import UserList from "./Components/Backoffice/Users/UserList";
+import OrganizationForm from "./Components/Backoffice/Organization/OrganizationForm";
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <BackofficeLayout> 
-          <Switch>
-            <Route exact path="/backoffice/users" component={UserList} />
-            <Route path="/backoffice/organization/edit" component={OrganizationForm} />
-            </Switch>
-          </BackofficeLayout>
+        <Switch>
           
+          <BackofficeLayout>
+            <Route exact path="/backoffice/users" component={UserList} />
+            <Route
+              path="/backoffice/organization/edit"
+              component={OrganizationForm}
+            />
+          </BackofficeLayout>
+
           <Layout>
-            <Switch>
-              <Route path="/about" component={About} />
-              <Route path="/contact" component={Contact} />
-            </Switch>
-          </Layout>   
-      
+            <Route path="/about" component={About} />
+            <Route path="/contact" component={Contact} />
+          </Layout>
+
+        </Switch>
       </BrowserRouter>
     </>
   );
