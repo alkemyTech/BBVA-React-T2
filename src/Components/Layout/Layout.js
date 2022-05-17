462e79ba2cd3750cb09c6e85f1eefc1064078a8e
Julia Scodelari <j.scodelari96@gmail.com>
2022-05-17 10:48:29 -0300

    resolucion de conflictos

--- src/Components/Layout/Layout.js
+++ src/Components/Layout/Layout.js
@@ -1,9 +1,5 @@
 import React from 'react';
-<<<<<<< HEAD
-//import Header from '../Frontoffice/Header/Header';
-=======
 import Header from '../Header/Header';
->>>>>>> 5b6059169cf398c31f87a04f49fd6b24054e47a8
 import Footer from '../Frontoffice/Footer/Footer';
 
 const Layout = ({children}) => {
