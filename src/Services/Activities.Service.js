350f5ee83ef0bf6a786165603140c3a11a2a1ea0
BrianCiszewski-bbva <brianmatias.ciszewskialonso@bbva.com>
2022-05-17 14:40:29 -0300

    estructura de las funciones create y update Activity

---
+++ src/Services/Activities.Service.js
@@ -0,0 +1,18 @@
+export function createActivity(activityBasics){
+    //La funcion devuelve un objeto con los campos de "Actividad"
+    //Params:
+    //activityBasics: espera un objeto con los campos basicos de la actividad.
+
+
+
+};
+
+export function updateActivity(id, activityBasics){
+    //La funcion devuelve objeto "Actividad" con la actividad del
+    //  id actualizada con los campos recibidos
+    //Params:
+    //activityBasics: espera un objeto con los campos basicos modificados
+
+
+    
+}
