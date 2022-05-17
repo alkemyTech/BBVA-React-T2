af4604bbaae8af743efe6db4798af8a5d6e7912e
variniaquisbert <variniaminerva.quisbertsoria@bbva.com>
2022-05-17 12:49:25 -0300

    add login & signup routes to App.js

--- src/App.js
+++ src/App.js
@@ -6,11 +6,12 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Layout from "./Components/Layout/Layout";
 import About from "./Components/Frontoffice/About/About";
 import Contact from "./Components/Frontoffice/Contact/Contact";
-
+import SignUp from "./Components/Frontoffice/SignUp/SignUp";
+import Login from './Components/Login/Login'
 //Backoffice
 import BackofficeLayout from "./Components/Layout/BackofficeLayout";
 import UserList from "./Components/Backoffice/Users/UserList";
- import OrganizationForm from "./Components/Backoffice/Organization/OrganizationForm";
+import OrganizationForm from "./Components/Backoffice/Organization/OrganizationForm";
 
 function App() {
   return (
@@ -30,6 +31,8 @@ function App() {
           <Route>
             <Layout>
               <Switch>
+                <Route path="/login" component={Login} />
+                <Route path="/signup" component={SignUp} />
                 <Route path="/about" component={About} />
                 <Route path="/contact" component={Contact} />
               </Switch>
