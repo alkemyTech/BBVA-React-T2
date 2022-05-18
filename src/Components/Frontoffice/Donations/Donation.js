0443a9710ec16a1876b6b9d8d4512c5ad9e773d6
Federico Amico <fede.amico98@gmail.com>
2022-05-18 18:04:22 -0300

    pop up funcionando sin validaciones

--- src/Components/Frontoffice/Donations/Donation.js
+++ src/Components/Frontoffice/Donations/Donation.js
@@ -18,14 +18,21 @@ const Donation = (props) => {
     const handleChange = (e) => {
         setInitialValues({...initialValues, [e.target.name] : e.target.value});
     }
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        history.push({
+            pathname: '/gracias',
+            state: { contribution: initialValues.amount}
+        });
+    }
 
     const handleClick = (e) => {
         e.preventDefault();
-        let monto = parseInt(prompt('cuanto dinero desea enviar?'));
-        history.push({
-            pathname: '/gracias',
-            state: { contribution: monto}
-        });
+        let popUp = document.getElementById('popUp');
+        popUp.classList.remove('pop-up-container-hidden');
+        popUp.classList.remove('pop-up-container-visible');
+        
     }
 
     return (
@@ -36,13 +43,14 @@ const Donation = (props) => {
                 contribuir
             </button>
         </div>
-        <div className='pop-up-container'>
-            <form className='pop-up-form'>
+        <div id='popUp' className='pop-up-container pop-up-container-hidden'>
+            <form className='pop-up-form' onSubmit={handleSubmit}>
                 <input className='pop-up-form__input' type='text' name='name' onChange={handleChange} placeholder='nombre'></input>
                 <input className='pop-up-form__input' type='text' name='creditNumber' onChange={handleChange} placeholder='numero de tarjeta'></input>
                 <input className='pop-up-form__input' type='text' name='id' onChange={handleChange} placeholder='numero de dni'></input>
                 <input className='pop-up-form__input' type='text' name='securityCode' onChange={handleChange} placeholder='codigo de seguridad'></input>
                 <input className='pop-up-form__input' type='text' name='expiration' onChange={handleChange} placeholder='fecha de vencimiento'></input>
+                <input className='pop-up-form__input' type='number' name='amount' onChange={handleChange} placeholder='Donacion'></input>
                 <button type='submit'>Donar</button>
             </form>
         </div>
