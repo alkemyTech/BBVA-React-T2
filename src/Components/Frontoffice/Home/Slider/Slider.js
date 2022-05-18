e77430db461adbd3a1c32a195127a6027e2afd3a
lautaropastor <lautaro.pastor@bbva.com>
2022-05-18 11:59:01 -0300

    Prop + query param variable

--- src/Components/Frontoffice/Home/Slider/Slider.js
+++ src/Components/Frontoffice/Home/Slider/Slider.js
@@ -2,13 +2,13 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import "./Slider.css"
 
-function Slider() {
+function Slider({length}) {
     const [slides, setSlides] = useState([]);
 
     const [currentItem, setCurrentItem] = useState(0);
 
     const getSlides = async () => {
-        const response = await axios.get("https://ongapi.alkemy.org/api/slides?limit=5")
+        const response = await axios.get(`https://ongapi.alkemy.org/api/slides?limit=${length}`)
         const slidesData = await response.data.data;
         setSlides(slidesData);
     }
