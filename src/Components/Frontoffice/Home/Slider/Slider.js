3c50982103ee7c9a400493fe8d4548e50d69a9da
lautaropastor <lautaro.pastor@bbva.com>
2022-05-18 11:53:16 -0300

    Petición GET Slides

--- src/Components/Frontoffice/Home/Slider/Slider.js
+++ src/Components/Frontoffice/Home/Slider/Slider.js
@@ -7,6 +7,17 @@ function Slider() {
 
     const [currentItem, setCurrentItem] = useState(0);
 
+    const getSlides = async () => {
+        const response = await axios.get("https://ongapi.alkemy.org/api/slides?limit=5")
+        const slidesData = await response.data.data;
+        setSlides(slidesData);
+    }
+
+    useEffect(() => {
+        getSlides();
+
+    }, [])
+
     return ( 
         <></>
      );
