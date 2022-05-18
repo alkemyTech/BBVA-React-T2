a32499c66e4720bd23358a06c33076234bb5b730
lautaropastor <lautaro.pastor@bbva.com>
2022-05-18 11:52:40 -0300

    Estados Slider

--- src/Components/Frontoffice/Home/Slider/Slider.js
+++ src/Components/Frontoffice/Home/Slider/Slider.js
@@ -3,6 +3,10 @@ import { useEffect, useState } from "react";
 import "./Slider.css"
 
 function Slider() {
+    const [slides, setSlides] = useState([]);
+
+    const [currentItem, setCurrentItem] = useState(0);
+
     return ( 
         <></>
      );
