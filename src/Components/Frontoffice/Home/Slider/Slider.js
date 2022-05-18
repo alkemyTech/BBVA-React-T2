48a121dc93335b494665a3f78485089445a97843
lautaropastor <lautaro.pastor@bbva.com>
2022-05-18 11:54:23 -0300

    Estructura HTML

--- src/Components/Frontoffice/Home/Slider/Slider.js
+++ src/Components/Frontoffice/Home/Slider/Slider.js
@@ -15,11 +15,33 @@ function Slider() {
 
     useEffect(() => {
         getSlides();
-
     }, [])
 
     return ( 
-        <></>
+        <div>
+            <div className="slider">
+                <div className="slider__wrapper">
+                    <div className="slider__inner" style={sliderInnerStyles}>
+                        {sliderItems}
+                    </div>
+                </div>
+                <div className="slider__indicators">
+                    {sliderIndicators}
+                </div>
+                <div className="slider__arrows">
+                    <span onClick={(e) => handleChange(e, "prev")} className="arrow arrow--prev">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="arrow__icon" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M15 19l-7-7 7-7" />
+                        </svg>
+                    </span>
+                    <span onClick={(e) => handleChange(e, "next")} className="arrow arrow--next">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="arrow__icon" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M9 5l7 7-7 7" />
+                        </svg>
+                    </span>
+                </div>
+            </div>
+        </div>
      );
 }
 
