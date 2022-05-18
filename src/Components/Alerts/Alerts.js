950a3fd9fec1fbb82b0af5bdd0bef19a6cf6be6e
Julia Scodelari <j.scodelari96@gmail.com>
2022-05-18 12:49:25 -0300

    creacion componente reutilizable de error e instalacion de sweetalerts

---
+++ src/Components/Alerts/Alerts.js
@@ -0,0 +1,8 @@
+import swAlert from 'sweetalert';
+
+//type can be: error - warning - success
+const alert = (title, message, type) => {
+    swAlert(title, message, type)
+}
+
+export default alert;
