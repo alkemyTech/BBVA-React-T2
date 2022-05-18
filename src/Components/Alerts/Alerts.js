9987a6e0588143a4cffc4aec3582872e7dd74f71
Federico Amico <fede.amico98@gmail.com>
2022-05-18 12:40:43 -0300

    modificacion para poder hacer un nuevo push. (solo elimine un ;)

--- src/Components/Alerts/Alerts.js
+++ src/Components/Alerts/Alerts.js
@@ -2,7 +2,7 @@ import swAlert from 'sweetalert';
 
 //type can be: error - warning - success
 const alert = (title, message, type) => {
-    swAlert(title, message, type);
+    swAlert(title, message, type)
 }
 
 export default alert;
