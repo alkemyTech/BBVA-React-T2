a033f5ad810fa80d2988ca16b918c7d8e65f3ead
Gustavo27Gonzalez <gustavo27gonzalez27@gmail.com>
2022-05-19 14:09:15 -0300

    Agregué spinner de loading y alerta en Activities para usuarios publicos

--- src/Components/Frontoffice/Activities/Activities.js
+++ src/Components/Frontoffice/Activities/Activities.js
@@ -1,23 +1,37 @@
 import Activity from './Activity'
 import { useState, useEffect } from 'react'
 import { Get } from '../../../Services/privateApiService';
+import Alert from '../../Alerts/Alerts.js';
+import Spinner from '../../Spinner/Spinner.js';
 
 const Activities = () => {
 
     const [activities, setActivities] = useState([]);
     const endpoint = process.env.REACT_APP_BASE_URL + process.env.REACT_APP_ACTIVITIES;
+    const [loader, setLoader] = useState(true);
+
+    const handleSpinner = () => {
+        setTimeout(() => setLoader(false), 1800);
+    }
+
     const getActivities = async () => {
         let res = await Get(endpoint);
-        setActivities(res.data.data)
+        try {
+            const res = await Get(endpoint);
+            setActivities(res.data.data);
+        } catch (err) {
+            Alert("Something went wrong. Please try again", "There was an error loading the Activities List", "error");
+        }
     }
 
     useEffect(() => {
+        handleSpinner();
         getActivities()
     },[setActivities])
-
+    
   return (
     <div className="general-container-activities"><br /><br />
-        {
+        {loader? <Spinner /> : (
             activities.map((activity) => {
                 return (
                     <Activity 
@@ -27,8 +41,8 @@ const Activities = () => {
                         key={activity.id} 
                     />
                 )
-            })
-        }
+            }))
+        }       
     </div>
   )
 }
