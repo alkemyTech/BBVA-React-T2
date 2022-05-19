219cb2011248160a5160541002eb524951b0cdcc
BrianCiszewski-bbva <brianmatias.ciszewskialonso@bbva.com>
2022-05-19 16:16:54 -0300

    loading en about

--- src/Components/Frontoffice/About/About.js
+++ src/Components/Frontoffice/About/About.js
@@ -2,14 +2,17 @@ import "./About.css";
 import { useState, useEffect } from "react";
 import  { Get }  from "../../../Services/publicApiService";
 import MembersAbout from "../Members/MembersAbout";
+import Spinner from "../../Spinner/Spinner";
 
 const About = () => {
 
   const [text, setText] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
 
   const fetchData = async () => {
     const res = await Get(process.env.REACT_APP_BASE_URL + process.env.REACT_APP_ORGANIZATION);
     setText(res.data.data.long_description);
+    setIsLoading(false);
   };
 
   useEffect(() => {
@@ -18,16 +21,20 @@ const About = () => {
   
   return (
     <>
-      <div className="container-about">
-        <div className="section-container-about">
-            <div className="main-text">
-                <h1 className="about-title">Nosotros</h1>
-                <div className="about-text">{text}</div>
+      {isLoading ? (<Spinner/>) : (
+        <>
+          <div className="container-about">
+            <div className="section-container-about">
+                <div className="main-text">
+                    <h1 className="about-title">Nosotros</h1>
+                    <div className="about-text">{text}</div>
+                </div>
+                <img src='./images/hands.png' className="about-img"/>
             </div>
-            <img src='./images/hands.png' className="about-img"/>
-        </div>
-      </div>
-      <MembersAbout/>
+          </div>
+          <MembersAbout/>
+        </>
+      )}
     </>
   );
 };
