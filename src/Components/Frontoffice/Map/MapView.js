08c46dec617392067c34a40b1d121477c2b18aa8
Federico Amico <fede.amico98@gmail.com>
2022-05-19 13:29:41 -0300

    componente MapView obtiene las cordenaads del usuario mediante la ubicacón del mismo. Utiliza las coordenadas ya establecidas de la ong y además coloca dos marcadores dentro del mapa

--- src/Components/Frontoffice/Map/MapView.js
+++ src/Components/Frontoffice/Map/MapView.js
@@ -6,10 +6,13 @@ import 'leaflet/dist/leaflet.css';
 import './MapView.css';
 import { IconLocation } from './IconLocation.js';
 
-const MapView = () => {
+const MapView = ({ coordinates }) => {
     const endpoint = process.env.REACT_APP_BASE_URL + process.env.REACT_APP_ORGANIZATION;
 
-    const [coords, setCoords] = useState();
+    const [coordsONG, setCoordsONG] = useState({
+        lat: '-34.597486832988814',
+        lng: '-58.37740350239873'
+    });
 
     const getDirections = async () => {
         let res = await Get(endpoint);
@@ -19,12 +22,16 @@ const MapView = () => {
     useEffect(() => {
         getDirections();
     }, [])
+    
     return (
-        <MapContainer center={{lat:'-34.597486832988814', lng:'-58.37740350239873'}} zoom={13}>
+        <MapContainer center={{lat: coordsONG.lat, lng: coordsONG.lng}} zoom={13}>
             <TileLayer url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
                 attribution= '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' 
             />
-            <Marker position={{lat: '-34.6259387', lng: '-58.4535433'}} 
+            <Marker position={{lat: coordsONG.lat, lng: coordsONG.lng}} 
+               icon={IconLocation} 
+            />
+            <Marker position={{lat: coordinates.latitude, lng: coordinates.longitude}} 
                icon={IconLocation} 
             />
         </MapContainer>
